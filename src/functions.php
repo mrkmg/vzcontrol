2428181570e4d40dd87ed4d58dd4580f71ee77a6
Kevin <kgravier@fluctuationsoftware.com>
2012-11-08 13:17:31 -0500

    TESTING

--- src/functions.php
+++ src/functions.php
@@ -38,7 +38,7 @@ function runSSH($server_name,$command){
              . (isset($servers[$server_name]['port'])?' -p '.$servers[$server_name]['port']:'')
              . ' -o ConnectTimeout=2 root@'
              . $servers[$server_name]['host']
-             . ' "'.str_replace('"','\\"',$command).'"';
+             . ' "TERM=xterm; '.str_replace('"','\\"',$command).'"';
     //putLine($command);
     passthru($command,$return);
     $reader->setStty();
